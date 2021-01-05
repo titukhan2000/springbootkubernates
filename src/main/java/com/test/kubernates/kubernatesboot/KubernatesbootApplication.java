bd713c8c63433c322cedb4d298ece9e805d3fd04
Govindaraj Subramani <govindaraj_subramani@gap.com>
2021-01-05 20:47:54 +0530

    Initial commit

---
+++ src/main/java/com/test/kubernates/kubernatesboot/KubernatesbootApplication.java
@@ -0,0 +1,25 @@
+package com.test.kubernates.kubernatesboot;
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public 
+
+class KubernatesbootApplication {
+
+	
+	public static void main(String[] args) {
+
+		SpringApplication.run(KubernatesbootApplication.class, args);
+	
+	}
+
+
+
+
+}
---
+++ src/test/java/com/test/kubernates/kubernatesboot/KubernatesbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kubernates.kubernatesboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
